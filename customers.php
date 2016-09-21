449895b4fc359c9b819dfae359bc92e92c3803c6
nikonst <nikonst@otenet.gr>
2016-09-21 20:27:23 +0300

    first commit

---
+++ customers.php
@@ -0,0 +1,103 @@
+{
+  "customers": [
+    {
+      "first_name": "Aleshia",
+	  "last_name": "Tomkiewicz",
+	  "company_name": "Alan D Rosenburg Cpa",
+	  "address": "14 Taylor St",
+	  "city":"St. Stephens Ward",
+	  "county":"Kent",
+	  "phone":"01835-703597",
+	  "email":"atomkiewicz@hotmail.com"
+    },
+
+	{
+      "first_name": "Evan",
+	  "last_name": "Zigomalas",
+	  "company_name": "Cap Gemini America",
+	  "address": "5 Binney St",
+	  "city":"Abbey Ward",
+	  "county":"Buckinghamshire",
+	  "phone":"01937-864715",
+	  "email":"evan.zigomalas@gmail.com"
+    },
+	
+	{
+      "first_name": "Andrade",
+	  "last_name": "Elliott",
+	  "company_name": "John W Esq",
+	  "address": "8 Moor St",
+	  "city":"East Southbourne",
+	  "county":"Bournemouth",
+	  "phone":"01347-368222",
+	  "email":"andrade@hotmail.com"
+    },
+	
+	{
+      "first_name": "Ulysses",
+	  "last_name": "Mcwalters",
+	  "company_name": "Mcmahan",
+	  "address": "505 Exeter Rd",
+	  "city":"Hawerby",
+	  "county":"Lincolnshire",
+	  "phone":"01912-771311",
+	  "email":"ulysses@hotmail.com"
+    },
+	
+	{
+      "first_name": "Tyisha",
+	  "last_name": "Veness",
+	  "company_name": "Champagne Room",
+	  "address": "5396 Forth Street",
+	  "city":"Greets Green",
+	  "county":"West Midlands",
+	  "phone":"01547-429341",
+	  "email":"tyisha.veness@hotmail.com"
+    },
+
+	{
+      "first_name": "Eric",
+	  "last_name": "Rampy",
+	  "company_name": "Thompson",
+	  "address": "9472 Lind St",
+	  "city":"Desborough",
+	  "county":"Northamptonshire",
+	  "phone":"01969-886290",
+	  "email":"erampy@rampy.co.uk"
+    },
+	
+	{
+      "first_name": "Marg",
+	  "last_name": "Grasmick",
+	  "company_name": "Wrangle Hill Auto Auct & Slvg",
+	  "address": "7457 Cowl St",
+	  "city":"Bargate Ward",
+	  "county":"Southampton",
+	  "phone":"01865-582516",
+	  "email":"marg@hotmail.com"
+    },
+	
+	{
+      "first_name": "Laquita",
+	  "last_name": "Hisaw",
+	  "company_name": "In Communications Inc",
+	  "address": "20 Gloucester St",
+	  "city":"Chirton Ward",
+	  "county":"Tyne & Wear",
+	  "phone":"01746-394243",
+	  "email":"laquita@yahoo.com"
+    },
+	
+	{
+      "first_name": "Yuette",
+	  "last_name": "Klapec",
+	  "company_name": "Max Video",
+	  "address": "45 Bradfield St",
+	  "city":"Parwich",
+	  "county":"Derbyshire",
+	  "phone":"01903-649460",
+	  "email":"yuette.klapec@klapec.co.uk"
+    }
+]
+}
+
